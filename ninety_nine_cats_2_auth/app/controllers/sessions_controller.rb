21356d89923e91c5f6acc0b4095a009c98f4d2d9
jackcusick95 <jackcusick95@gmail.com>
2021-04-05 16:03:54 -0400

    Updated Application HTML and sessions controller

--- ninety_nine_cats_2_auth/app/controllers/sessions_controller.rb
+++ ninety_nine_cats_2_auth/app/controllers/sessions_controller.rb
@@ -16,6 +16,13 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    current_user.reset_session_token! if logged_in?
+    session[:session_token] = nil 
+    @current_user = nil 
+    redirect_to new_session_url 
+  end 
+
   
 
 end
